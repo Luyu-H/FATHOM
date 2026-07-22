c1145e76ba6f7db5059aa11bf047cb2a51d93676
Luyu-H <hanluyu1030@gmail.com>
2026-07-22 19:18:00 +0800

    fix script for downloading data

--- data/scientific_database/test/download_testset.py
+++ data/scientific_database/test/download_testset.py
@@ -3,6 +3,6 @@ from huggingface_hub import snapshot_download
 snapshot_download(
     repo_id="Luyu-H/CMIP6_scientific_database_for_FATHOM",
     repo_type="dataset",
-    allow_patterns="test",
+    allow_patterns="test/*",
     local_dir="data/scientific_database/test"
 )
